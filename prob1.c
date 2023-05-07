5d16c1fc669e4e1005bb8232bcb7b52d45025c85
spikecowboybebop <132824450+spikecowboybebop@users.noreply.github.com>
2023-05-07 18:28:03 +0600

    Add files via upload

---
+++ prob1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+// Ascending Order Sorting
+int main() {
+    int size;
+    printf("Enter array size: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for(int i = 0; i < size; i++) {
+        printf("Enter array element %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+    int s = 0;
+    // Loop for calculating the maximum number of time the sorting loop would need to run.
+    for(int i = 1; i < size; i++) {
+            s = s + (size - i);
+    }
+    // Sorting Loop
+    for(int i = 1; i <= s; i++) {
+        for(int j = 0; j < size; j++) {
+            if(arr[j] > arr[j+1]) {
+                int c = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = c;
+        }
+            else {
+                continue;
+            }
+        }
+    }
+    //Printing the loop
+    printf("The array elements are: ");
+    for(int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
