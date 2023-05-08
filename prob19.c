6955c4cad0608d1f535def2b14bd180f7476ba3c
spikecowboybebop <132824450+spikecowboybebop@users.noreply.github.com>
2023-05-08 20:34:00 +0600

    Add files via upload

--- prob19.c
+++ prob19.c
@@ -6,27 +6,28 @@ int main() {
     int mat2[100][100];
     int matsum[100][100];
 
+    printf("[A - B] Matrix Subtraction\n");
     printf("Input size of the square matrix (Less than 5): ");
     scanf("%d", &size);
 
-    printf("Input Elements in the first matrix: \n");
+    printf("Input Elements in the matrix A: \n");
     for(int i = 0; i < size; i++) {
         for(int k = 0; k < size; k++) {
             printf("Element [%d][%d]: ", i, k);
             scanf("%d", &mat1[i][k]);
         }
     }
-    printf("Input Elements in the second matrix: \n");
+    printf("Input Elements in the matrix B: \n");
     for(int i = 0; i < size; i++) {
         for(int k = 0; k < size; k++) {
             printf("Element [%d][%d]: ", i, k);
             scanf("%d", &mat2[i][k]);
         }
     }
-    printf("The addition of two matrix is: \n");
+    printf("A - B = \n");
     for(int i = 0; i < size; i++) {
         for(int k = 0; k < size; k++) {
-            matsum[i][k] = mat1[i][k] + mat2[i][k];
+            matsum[i][k] = mat1[i][k] - mat2[i][k];
             printf("%d ", matsum[i][k]);
         }
         printf("\n");
