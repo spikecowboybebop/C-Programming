f28e72f173166e4b7b17942dd271bc06e0b95638
spikecowboybebop <132824450+spikecowboybebop@users.noreply.github.com>
2023-05-09 20:37:12 +0600

    Add files via upload

---
+++ prob24.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int A[100][100];
+
+    int size;
+    printf("Input size of square matrix: ");
+    scanf("%d", &size);
+
+    for(int i = 0; i < size; i++) {
+        for(int k = 0; k < size; k++) {
+            printf("Input array element A[%d][%d] - ", i, k);
+            scanf("%d", &A[i][k]);
+        }
+    }
+
+    int sum = 0;
+    for(int i = (size - 1); i >= 0; i--) {
+        sum = sum + A[i][i];
+    }
+
+    printf("The sum of left diagonal elements is: %d", sum);
+
+
+    return 0;
+}
